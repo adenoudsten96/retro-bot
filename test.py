e33eee972e04cb757053302b357f86780bb92abb
adenoudsten96 <mail@alexonline.nl>
2021-08-06 19:59:12 +0200

    Added loot sheet base

--- test.py
+++
@@ -1,4 +0,0 @@
-s = "Created by Numilex on 27/09 \nUse the buttons below to join/leave. Creator can ping/DM all gamers in group.\nClick skull to remove."
-s = s.split("\n")
-s = s[0].split(" ")[2:][:-3]
-print(s)
