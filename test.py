168760b1c7990b8fb103cf6ca1bd37098b4c328f
adenoudsten96 <mail@alexonline.nl>
2021-08-06 19:58:30 +0200

    Revert "Added loot sheet script base"
    This reverts commit dce09d05567ca8ead817ab034760906563437705.

--- test.py
+++ test.py
@@ -1,26 +1,4 @@
-from discord.ext.commands import Bot
-from discord_components import DiscordComponents, Button
-
-bot = Bot(command_prefix = "/")
-
-
-@bot.event
-async def on_ready():
-    DiscordComponents(bot)
-    print(f"Logged in as {bot.user}!")
-
-
-@bot.command()
-async def button(ctx):
-    await ctx.send(
-        "Hello, World!",
-        components = [
-            Button(label = "WOW button!")
-        ]
-    )
-
-    interaction = await bot.wait_for("button_click", check = lambda i: i.component.label.startswith("WOW"))
-    await interaction.respond(content = "Button clicked!")
-
-
-bot.run("NzQ1OTc4MDEyNjkyMTE5NjQy.Xz5oKQ.RMgs8xbbihrZR5E6VfeC8YRDmHU")
+s = "Created by Numilex on 27/09 \nUse the buttons below to join/leave. Creator can ping/DM all gamers in group.\nClick skull to remove."
+s = s.split("\n")
+s = s[0].split(" ")[2:][:-3]
+print(s)
